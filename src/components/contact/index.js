91ae200a68cafa182d1cb2367480deef276ea24b
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-17 23:43:56 -0300

    Added Detail component

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,6 +1,7 @@
 import List from "./List";
+import Details from "./Details";
 import Add from "./Add";
 import Update from "./Update";
 import Remove from "./Remove";
 
-export { List, Add, Update, Remove };
+export { List, Details, Add, Update, Remove };
