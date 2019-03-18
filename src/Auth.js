5778ede3bab4d4b65efc3c757cff7cfbe14a013e
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-17 23:33:46 -0300

    Created functions to authenticate

--- src/Auth.js
+++ src/Auth.js
@@ -1,1 +1,13 @@
-export const isAuthenticated = () => true;
+export const TOKEN_KEY = "@contact-list-Token";
+
+export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
+
+export const login = token => {
+  localStorage.setItem(TOKEN_KEY, token);
+};
+
+export const logout = () => {
+  localStorage.removeItem(TOKEN_KEY);
+};
