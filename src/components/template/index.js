4c3532bea0c5f5be65c895fcc2483897a10f16a2
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-17 23:42:36 -0300

    Created template

---
+++ src/components/template/index.js
@@ -0,0 +1,80 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
+} from "@material-ui/core";
+import { ListAlt, ArrowBackIos, Close } from "@material-ui/icons";
+import { withStyles } from "@material-ui/core/styles";
+
+import { logout } from "../../Auth";
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  closeButton: {
+    marginRight: -12,
+    marginLeft: 20
+  },
+  body: {
+    marginTop: 56,
+    width: "100%"
+  }
+};
+
+const Template = ({ history, location, children, classes }) => {
+  const logoutApp = () => {
+    logout();
+    history.push("/");
+  };
+
+  return (
+    <React.Fragment>
+      <AppBar position="static" style={{ position: "fixed" }}>
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            onClick={() => history.goBack()}
+          >
+            {location.pathname === "/contacts" ? <ListAlt /> : <ArrowBackIos />}
+          </IconButton>
+
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            CONTACTS APP
+          </Typography>
+          <IconButton
+            className={classes.closeButton}
+            color="inherit"
+            onClick={logoutApp}
+          >
+            <Close />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.body}>{children}</div>
+    </React.Fragment>
+  );
+};
+
+Template.propTypes = {
+  children: PropTypes.node.isRequired,
+  location: PropTypes.object.isRequired,
+  history: PropTypes.object,
+  classes: PropTypes.object
+};
+
+export default withStyles(styles)(withRouter(Template));
