cfcce15a4c501dd2f7029de57497258015df067d
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-17 23:35:12 -0300

    Change some routes and inserted template

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,18 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import { isAuthenticated } from "./Auth";
-import { Register, Login } from "./components/authentication";
-import { List, Add, Update, Remove } from "./components/contact";
+import { Signup, Signin } from "./components/authentication";
+import { List, Details, Add, Update, Remove } from "./components/contact";
+import Template from "../src/components/template";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={props =>
       isAuthenticated() ? (
-        <Component {...props} />
+        <Template>
+          <Component {...props} />
+        </Template>
       ) : (
         //   Prop state garante ao usuário que ele não perca seu histórico de rotas
         <Redirect to={{ pathname: "/", state: { from: props.location } }} />
@@ -30,12 +33,14 @@ const Routes = () => (
     <Switch>
       {/* Exact faz com que a rota seja exatamente a passada para o path.
         "/app" seria apenas /app e não /app/dashboard etc. */}
-      <Route exact path="/" component={() => <Login />} />
-      <Route exact path="/register" component={() => <Register />} />
+      <Route exact path="/" component={() => <Signin />} />
+      <Route exact path="/register" component={() => <Signup />} />
       <PrivateRoute exact path="/contacts" component={List} />
       <PrivateRoute exact path="/contacts/add" component={Add} />
-      <PrivateRoute exact path="/contacts/update" component={Update} />
       <PrivateRoute exact path="/contacts/remove" component={Remove} />
+      <PrivateRoute exact path="/contacts/:id" component={Details} />
+      <PrivateRoute exact path="/contacts/:id/update" component={Update} />
+      <Route path="*" component={() => <h1>Page not found!</h1>} />
     </Switch>
   </BrowserRouter>
 );
