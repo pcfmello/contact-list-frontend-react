0a533e56085ecbe0d7dfd1d7d8dd49c65afe8c9e
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-17 23:32:56 -0300

    Inserting class to styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import PropTypes from "prop-types";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { withStyles } from "@material-ui/core/styles";
 
 import Routes from "./routes";
 
-const App = () => (
-  <div className="App" style={{ minWidth: 320}}>
+const styles = {
+  root: {
+    minWidth: 320,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  }
+};
+
+const App = ({ classes }) => (
+  <div className={classes.root}>
     <CssBaseline />
     <Routes />
   </div>
 );
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(App);
